a03020ad0d04822959f1dfb2a6a628b15cc4937c
miDuarte <mylenaduarte98@gmail.com>
2020-01-29 10:34:01 -0300

    httpRequisition

---
+++ app/src/main/java/com/example/adiligencia01/HttpRequisition.java
@@ -0,0 +1,18 @@
+package com.example.adiligencia01;
+
+import android.os.AsyncTask;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class HttpRequisition extends AsyncTask<Void, Void, String>{
+  @RequiresApi(api = Build.VERSION_CODES.0)
+    @Override
+    protected String doInBackground
+}
+
